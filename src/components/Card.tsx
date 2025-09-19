4b21dc3017d5ab242d74205b7a456a34b0e2811a
Moses Valladares <mvallad1@binghamton.edu>
2025-09-18 21:01:21 -0400

    added in content for all tabs except about and contact

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,8 +1,25 @@
 
-const Card = () => {
+interface CardProps {
+  title: string;
+  description: string;
+  image: string;
+  link: string;
+}
+
+const Card = ({title, description, image, link }: CardProps) => {
   return (
-    <div>
-        Card
+    <div className="max-w-sm mx-auto m-[1rem] bg-[#1A1C1E] rounded-lg shadow-md overflow-hidden">
+        <img src={image} alt={title} className="w-full h-48 object-cover" />
+
+        <div className="p-6">
+          <h2 className="text-2xl font-bold mb-2">{title}</h2>
+          <p className="text-gray-700 mb-4">{description}</p>
+
+          <a href={link} className="inline-block px-4 py-2 bg-white text-black font-semibold rounded-lg shadow hover:bg-gray-600 hover:text-white transition duration-200">
+            Learn More
+          </a>
+
+        </div>
     </div>
   )
 }
--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -73,9 +73,24 @@ const tabs = [
 ];
 
 const Tab = () => {
+  const [activeTab, setActiveTab] = useState(tabs[0].id);
   return (
-    <div>
-        Tab
+    <div className="p-4 mt-[3rem]">
+      <div className="flex border-b border-gray-200">
+        {tabs.map((tab) => (
+          <button key={tab.id} className={`flex-1 text-center py-2 px-4 font-medium text-sn ${activeTab === tab.id ? 
+            'border-b-2' : 'text-gray-600'}`} onClick={() => setActiveTab(tab.id)}>
+            
+            <div className="flex items-center justify-center space-x-2">
+              {tab.icon}
+              <span>{tab.label}</span>
+            </div>
+          </button>
+        ))}
+      </div>
+        <div className="mt-4-p-4 rounded-lg">
+          {tabs.find((tab) => tab.id === activeTab)?.content}
+        </div>
     </div>
   )
 }
